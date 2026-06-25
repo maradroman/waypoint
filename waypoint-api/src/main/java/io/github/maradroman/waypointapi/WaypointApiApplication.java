e11172f8e98317ef1b85f019859fdb9a98c3d71b
maradroman <maradroman2@gmail.com>
2026-06-25 14:16:57 +0300

    Create BE/FE modules

---
+++ waypoint-api/src/main/java/io/github/maradroman/waypointapi/WaypointApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.maradroman.waypointapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaypointApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaypointApiApplication.class, args);
+    }
+
+}
---
+++ waypoint-api/src/test/java/io/github/maradroman/waypointapi/WaypointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maradroman.waypointapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaypointApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
