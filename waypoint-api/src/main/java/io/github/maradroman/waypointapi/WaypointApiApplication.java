e4527201b082bedc322dc5e36411e3fdad7b1f52
maradroman <maradroman2@gmail.com>
2026-06-25 17:22:13 +0300

    tests

--- waypoint-api/src/main/java/io/github/maradroman/waypointapi/WaypointApiApplication.java
+++ waypoint-api/src/main/java/io/github/maradroman/waypointapi/WaypointApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WaypointApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WaypointApiApplication.class, args);
     }
 
